a3d7a11dc21851b44b894beaf0dd68863b6f5b3b
StanleyKayzz <ondzoungabruce@hotmail.fr>
2022-09-29 00:40:38 +0200

    updating project to tsx

---
+++ rhm-website/src/App.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import Footer from './Footer';
+
+// import Button from 'react-bootstrap/Button';
+// import Container from 'react-bootstrap/Container';
+// import Form from 'react-bootstrap/Form';
+// import Nav from 'react-bootstrap/Nav';
+// import Navbar from 'react-bootstrap/Navbar';
+// import NavDropdown from 'react-bootstrap/NavDropdown';
+
+import Header from './Header';
+
+function App() {
+  
+  return (
+    <div className='App'>
+<Header></Header>
+      App
+<Footer></Footer>
+
+    </div>
+  );
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.tsx</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //   </div>
+  // );
+}
+
+export default App;
